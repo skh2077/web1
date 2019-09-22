9a730bf203a2590be8a17502d3c579920d676742
rbqbwbrb <rbqbwbrb@daum.net>
2019-09-23 03:44:05 +0900

    Revert "no message"
    This reverts commit b15330895807c2268a63c45ec6c406793ff74745.

--- colors.js
+++ colors.js
@@ -1,22 +1,19 @@
 var Links = {
   setColor:function (color){
-    // var alist = document.querySelectorAll('a');
-    // var i = 0;
-    // while(i<alist.length){
-    //   alist[i].style.color = color;
-    //   i = i + 1;
-    // }
-    $('a').css('color', color);
+    var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i<alist.length){
+      alist[i].style.color = color;
+      i = i + 1;
+    }
   }
 }
 var Body = {
   setColor: function(color){
-    // document.querySelector('body').style.color = color;
-    $('body').css('color',color);
+    document.querySelector('body').style.color = color;
   },
   setBackgroundColor:function(color){
-    // document.querySelector('body').style.backgroundColor = color;
-    $('body').css('backgroundColor', color);
+    document.querySelector('body').style.backgroundColor = color;
   }
 }
 function nightDayHandler(self){
