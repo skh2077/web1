922afb76d4c46a19200cb38f0df24767e79f0c96
apstr <skh20000017@gmail.com>
2021-03-03 09:41:21 +0900

    Add files via upload

---
+++ chatbot.js
@@ -0,0 +1,8 @@
+const dfMessenger = document.querySelector('df-messenger');
+dfMessenger.addEventListener('df-messenger-loaded', function (event) {
+  // Handle event
+  dfMessenger.renderCustomText('Custom text');
+
+
+});
+
